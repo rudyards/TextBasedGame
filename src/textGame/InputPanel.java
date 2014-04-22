1f96e0fa9c30e537f0f713e51a3a4d6543156cf8
Rudy Summers <rudy@mightycheese.com>
2014-04-22 11:46:45 -0700

    Reformatted inputPanel
    Improved the inputPanel by converting it from true text based to choice
    based. Added the first choice, and functionality to make it one use
    only.

--- src/textGame/InputPanel.java
+++ src/textGame/InputPanel.java
@@ -11,37 +11,83 @@ import javax.swing.JLabel;
 public class InputPanel extends JPanel {
 	private String currentAction;
 	private JTextArea Reaction;
+	private JButton choice1;
+	private JButton choice2;
+	private JButton choice3;
+	private JButton choice4;
+	private int currentChoice;
 
 	/**
 	 * Create the panel.
 	 */
 	public InputPanel() {
-		currentAction = "";
-		
+
 		setLayout(null);
-		
+
 		Reaction = new JTextArea();
 		Reaction.setEditable(false);
 		Reaction.setLineWrap(true);
 		Reaction.setBounds(18, 26, 415, 155);
 		add(Reaction);
-		
-		JButton choice1 = new JButton("Choice One");
-		choice1.setBounds(42, 212, 117, 29);
+
+		currentChoice = 0;
+		// Button text is instantiated for their initial choice
+		final JButton choice1 = new JButton("Dikaiopolis");
+		choice1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (currentChoice == 0) {
+					Reaction.setText("Your name is now Dikaiopolis");
+					currentChoice++;
+				}
+			}
+		});
+		choice1.setBounds(6, 193, 117, 29);
 		add(choice1);
-		
-		JButton choice2 = new JButton("Choice Two");
-		choice2.setBounds(42, 253, 117, 29);
+
+		final JButton choice2 = new JButton("Huphantes");
+		choice2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (currentChoice == 0) {
+					Reaction.setText("Your name is now Huphantes");
+					currentChoice++;
+				}
+			}
+		});
+
+		choice2.setBounds(6, 265, 117, 29);
 		add(choice2);
-		
-		JButton choice3 = new JButton("Choice Three");
-		choice3.setBounds(258, 212, 117, 29);
+
+		final JButton choice3 = new JButton("Axaitios");
+		choice3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (currentChoice == 0) {
+					Reaction.setText("Your name is now Axaitios");
+					currentChoice++;
+				}
+			}
+		});
+
+		choice3.setBounds(327, 193, 117, 29);
 		add(choice3);
-		
-		JButton choice4 = new JButton("Choice Four");
-		choice4.setBounds(258, 253, 117, 29);
+
+		final JButton choice4 = new JButton("Eruthros");
+		choice4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (currentChoice == 0) {
+					Reaction.setText("Your name is now Eruthros");
+					currentChoice++;
+				}
+			}
+		});
+
+		choice4.setBounds(327, 265, 117, 29);
 		add(choice4);
-		
+
+		start();
+	}
+
+	public void start() {
+		Reaction.setText("What is your name?");
 
 	}
 }
