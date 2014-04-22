2f425e08ec5d271b7c42ae523ae38586acf2a121
Rudy Summers <rudy@mightycheese.com>
2014-04-22 12:26:53 -0700

    MADE THE GAME ACTUALLY WORK
    Fixed a bug which was preventing functionality, and added the first ~2
    choices.

--- src/textGame/InputPanel.java
+++ src/textGame/InputPanel.java
@@ -16,6 +16,8 @@ public class InputPanel extends JPanel {
 	private JButton choice3;
 	private JButton choice4;
 	private int currentChoice;
+	private String name;
+	private String general;
 
 	/**
 	 * Create the panel.
@@ -30,26 +32,39 @@ public class InputPanel extends JPanel {
 		Reaction.setBounds(18, 26, 415, 155);
 		add(Reaction);
 
-		currentChoice = 0;
 		// Button text is instantiated for their initial choice
-		final JButton choice1 = new JButton("Dikaiopolis");
+		choice1 = new JButton("Choice One");
 		choice1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (currentChoice == 0) {
-					Reaction.setText("Your name is now Dikaiopolis");
+					name = "Dikaiopolis";
 					currentChoice++;
+					generals();
+				}else if(currentChoice == 1){
+					general = "Achilles";
+					currentChoice++;
+					talents();
+				}else{
+					start();
 				}
 			}
 		});
 		choice1.setBounds(6, 193, 117, 29);
 		add(choice1);
 
-		final JButton choice2 = new JButton("Huphantes");
+		choice2 = new JButton("Choice Two");
 		choice2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (currentChoice == 0) {
-					Reaction.setText("Your name is now Huphantes");
+					name = "Huphantes";
 					currentChoice++;
+					generals();
+				}else if(currentChoice == 1){
+					general = "Agammenmon";
+					currentChoice++;
+					talents();
+				}else{
+					start();
 				}
 			}
 		});
@@ -57,12 +72,19 @@ public class InputPanel extends JPanel {
 		choice2.setBounds(6, 265, 117, 29);
 		add(choice2);
 
-		final JButton choice3 = new JButton("Axaitios");
+		choice3 = new JButton("Choice Three");
 		choice3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (currentChoice == 0) {
-					Reaction.setText("Your name is now Axaitios");
+					name = "Axaitios";
 					currentChoice++;
+					generals();
+				}else if(currentChoice == 1){
+					general = "Odysseus";
+					currentChoice++;
+					talents();
+				}else{
+					start();
 				}
 			}
 		});
@@ -70,12 +92,19 @@ public class InputPanel extends JPanel {
 		choice3.setBounds(327, 193, 117, 29);
 		add(choice3);
 
-		final JButton choice4 = new JButton("Eruthros");
+		choice4 = new JButton("Choice Four");
 		choice4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (currentChoice == 0) {
-					Reaction.setText("Your name is now Eruthros");
+					name = "Eruthros";
 					currentChoice++;
+					generals();
+				}else if(currentChoice == 1){
+					general = "Diomedes";
+					currentChoice++;
+					talents();
+				}else{
+					start();
 				}
 			}
 		});
@@ -83,11 +112,43 @@ public class InputPanel extends JPanel {
 		choice4.setBounds(327, 265, 117, 29);
 		add(choice4);
 
-		start();
+		preGame();
+	}
+	public void preGame(){
+		Reaction.setText("Please hit any choice to start.");
+		
+		currentChoice = -1;
+	}
+	
+	public void start() {
+		choice1.setText("Dikaiopolis");
+		choice2.setText("Huphantes");
+		choice3.setText("Axaitios");
+		choice4.setText("Eruthros");
+		
+		
+		Reaction.setText("What is your name?");
+		currentChoice = 0;
 	}
 
-	public void start() {
-		Reaction.setText("What is your name?");
+	public void generals() {
+		choice1.setText("Achilles");
+		choice2.setText("Agammemnon");
+		choice3.setText("Odysseyus");
+		choice4.setText("Diomedes");
+
+		Reaction.setText("Ah, "
+				+ name
+				+ ". An excellent name. \n You came to Troy in service of one of the following generals.");
+	}
+	public void talents(){
+		choice1.setText("Agility");
+		choice2.setText("Luck");
+		choice3.setText("Stregnth");
+		choice4.setText("Intellegence");
+		
+		Reaction.setText("You came to Troy in service of "+general+".");
+		
 
 	}
 }
