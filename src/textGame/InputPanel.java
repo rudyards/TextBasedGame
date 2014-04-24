b8a1cba81795639bc04129e0c2ecd8ac33063a0e
Rudy Summers <rudy@mightycheese.com>
2014-04-24 11:02:23 -0700

    Added Validity Check, Added First Round Combat
    Checks to see the button’s text before any action. If the button says
    “N/A," it avoids crashing and outputs text. Furthermore, added the
    first round of combat.

--- src/textGame/InputPanel.java
+++ src/textGame/InputPanel.java
@@ -19,6 +19,7 @@ public class InputPanel extends JPanel {
 	private String name;
 	private String general;
 	private String talent;
+	private boolean injured;
 
 	/**
 	 * Create the panel.
@@ -46,31 +47,43 @@ public class InputPanel extends JPanel {
 		 */
 		choice1 = new JButton("Choice One");
 		choice1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (currentChoice == 0) {
-					name = "Dikaiopolis";
-					currentChoice++;
-					generals();
-				} else if (currentChoice == 1) {
-					general = "Achilles";
-					currentChoice++;
-					talents();
-				} else if (currentChoice == 2) {
-					talent = "Agility";
-					currentChoice++;
-					level1();
-				} else if (currentChoice == 3) {
-					// output result of choice, then give counterattack
-					// opportunity
-
-					counterAttack();
-
-				} else if (currentChoice == 4) {
-					// output result of choice, then return to camp (if survive)
-
-					exploreC();
+			public void actionPerformed(ActionEvent e) {
+				if (choice1.getText().equals("N/A")) {
+					System.out.println("Not a valid choice. Try again.");
 				} else {
-					start();
+					if (currentChoice == 0) {
+						name = "Dikaiopolis";
+						currentChoice++;
+						generals();
+					} else if (currentChoice == 1) {
+						general = "Achilles";
+						currentChoice++;
+						talents();
+					} else if (currentChoice == 2) {
+						talent = "Agility";
+						currentChoice++;
+						level1();
+					} else if (currentChoice == 3) {
+						// output result of choice, then give counterattack
+						// opportunity
+						if (talent.equals("Combat")) {
+							Reaction.setText("You block his sword, and turn it to the side with relative\nease.");
+						} else if (talent.equals("Luck")) {
+							Reaction.setText("Your swords clash, and his shatters. You met blades\nalong a crack in his weapon. Luck strikes again.");
+						} else {
+							Reaction.setText("His attack is stronger than your block, and you are forced back.\nHe follows up with a quick stab that slices open your side.\nPain rips through you.");
+							injured = true;
+						}
+						counterAttack();
+
+					} else if (currentChoice == 4) {
+						// output result of choice, then return to camp (if
+						// survive)
+
+						exploreC();
+					} else {
+						start();
+					}
 				}
 			}
 		});
@@ -79,30 +92,40 @@ public class InputPanel extends JPanel {
 
 		choice2 = new JButton("Choice Two");
 		choice2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (currentChoice == 0) {
-					name = "Huphantes";
-					currentChoice++;
-					generals();
-				} else if (currentChoice == 1) {
-					general = "Agammenmon";
-					currentChoice++;
-					talents();
-				} else if (currentChoice == 2) {
-					talent = "Luck";
-					currentChoice++;
-					level1();
-				} else if (currentChoice == 3) {
-					// output result of choice, then give counterattack
-					// opportunity
-
-					counterAttack();
-				} else if (currentChoice == 4) {
-					// output result of choice, then return to camp (if survive)
-
-					exploreC();
+			public void actionPerformed(ActionEvent e) {
+				if (choice2.getText().equals("N/A")) {
+					System.out.println("Not a valid choice. Try again.");
 				} else {
-					start();
+					if (currentChoice == 0) {
+						name = "Huphantes";
+						currentChoice++;
+						generals();
+					} else if (currentChoice == 1) {
+						general = "Agammenmon";
+						currentChoice++;
+						talents();
+					} else if (currentChoice == 2) {
+						talent = "Luck";
+						currentChoice++;
+						level1();
+					} else if (currentChoice == 3) {
+						// output result of choice, then give counterattack
+						// opportunity
+						if (talent.equals("Agility")) {
+							Reaction.setText("You nimbly step aside, and line yourself up to counter attack.");
+						} else {
+							Reaction.setText("You weren't quite quick enough, and his sword rips along your upper leg. Your eyes blur.");
+							injured = true;
+						}
+						counterAttack();
+					} else if (currentChoice == 4) {
+						// output result of choice, then return to camp (if
+						// survive)
+
+						exploreC();
+					} else {
+						start();
+					}
 				}
 			}
 		});
@@ -112,31 +135,41 @@ public class InputPanel extends JPanel {
 
 		choice3 = new JButton("Choice Three");
 		choice3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (currentChoice == 0) {
-					name = "Axaitios";
-					currentChoice++;
-					generals();
-				} else if (currentChoice == 1) {
-					general = "Odysseus";
-					currentChoice++;
-					talents();
-				} else if (currentChoice == 2) {
-					talent = "Might";
-					currentChoice++;
-					level1();
-				} else if (currentChoice == 3) {
-					// output result of choice, then give counterattack
-					// opportunity
-
-					counterAttack();
-
-				} else if (currentChoice == 4) {
-					// output result of choice, then return to camp (if survive)
-
-					exploreC();
+			public void actionPerformed(ActionEvent e) {
+				if (choice3.getText().equals("N/A")) {
+					System.out.println("Not a valid choice. Try again.");
 				} else {
-					start();
+					if (currentChoice == 0) {
+						name = "Axaitios";
+						currentChoice++;
+						generals();
+					} else if (currentChoice == 1) {
+						general = "Odysseus";
+						currentChoice++;
+						talents();
+					} else if (currentChoice == 2) {
+						talent = "Might";
+						currentChoice++;
+						level1();
+					} else if (currentChoice == 3) {
+						// output result of choice, then give counterattack
+						// opportunity
+						if (talent.equals("Luck")) {
+							Reaction.setText("He had you dead to rights, but a stray arrow from a skirmish downwind embeds itself in his arm. You got lucky.");
+						} else {
+							Reaction.setText("The sword slashes your shoulder, and you roar in pain.");
+							injured = true;
+						}
+						counterAttack();
+
+					} else if (currentChoice == 4) {
+						// output result of choice, then return to camp (if
+						// survive)
+
+						exploreC();
+					} else {
+						start();
+					}
 				}
 			}
 		});
@@ -146,31 +179,25 @@ public class InputPanel extends JPanel {
 
 		choice4 = new JButton("Choice Four");
 		choice4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (currentChoice == 0) {
-					name = "Eruthros";
-					currentChoice++;
-					generals();
-				} else if (currentChoice == 1) {
-					general = "Diomedes";
-					currentChoice++;
-					talents();
-				} else if (currentChoice == 2) {
-					talent = "Wits";
-					currentChoice++;
-					level1();
-				} else if (currentChoice == 3) {
-					// output result of choice, then give counterattack
-					// opportunity
-
-					counterAttack();
-
-				} else if (currentChoice == 4) {
-					// output result of choice, then return to camp (if survive)
-
-					exploreC();
+			public void actionPerformed(ActionEvent e) {
+				if (choice4.getText().equals("N/A")) {
+					System.out.println("Not a valid choice. Try again.");
 				} else {
-					start();
+					if (currentChoice == 0) {
+						name = "Eruthros";
+						currentChoice++;
+						generals();
+					} else if (currentChoice == 1) {
+						general = "Diomedes";
+						currentChoice++;
+						talents();
+					} else if (currentChoice == 2) {
+						talent = "Wits";
+						currentChoice++;
+						level1();
+					} else {
+						start();
+					}
 				}
 			}
 		});
@@ -205,7 +232,7 @@ public class InputPanel extends JPanel {
 
 		Reaction.setText("Ah, "
 				+ name
-				+ ". An excellent name. \n You came to Troy in service of one of the following generals.");
+				+ ". An excellent name. \nYou came to Troy in service of one of the following generals.");
 
 	}
 
@@ -217,13 +244,13 @@ public class InputPanel extends JPanel {
 
 		Reaction.setText("You came to Troy in service of " + general + ".");
 		if (general.equals("Achilles")) {
-			Reaction.append("\n The great Achilles is known for his invincibility in combat. You are in good hands.");
+			Reaction.append("\nThe great Achilles is known for his invincibility in combat. You \nare in good hands.");
 		} else if (general.equals("Agammemnon")) {
-			Reaction.append("Ah, the great king Agamemnon, the general of all the Greeks");
+			Reaction.append("\nAh, the great king Agamemnon, the general of all the Greeks");
 		} else if (general.equals("Diomedes")) {
-			Reaction.append("The legendary warrior king, known for felling two gods over the course of the war.");
-		} else if (general.equals("Odysseyus")) {
-			Reaction.append("Perhaps the most clever of all the greek generals, the Ithican Odysseyus.");
+			Reaction.append("\nThe legendary warrior king, known for felling two gods over the course of the war.");
+		} else if (general.equals("Odysseus")) {
+			Reaction.append("\nPerhaps the most clever of all the greek generals, \nthe Ithican Odysseyus.");
 		}
 
 		Reaction.append("\nWhat are you known for?");
@@ -231,34 +258,49 @@ public class InputPanel extends JPanel {
 
 	public void level1() {
 		if (talent.equals("Luck")) {
-			Reaction.setText("You're not personally blessed by Tyche, but you\n roll knucklebones among the best of them.");
+			Reaction.setText("You're not personally blessed by Tyche, but you \nroll knucklebones among the best of them.");
 		} else if (talent.equals("Agility")) {
-			Reaction.setText("Atalanta could outrun you any day of the week, but you\n move quick for someone of your size.");
+			Reaction.setText("Atalanta could outrun you any day of the week, but you move\nquick for someone of your size.\n\n");
 		} else if (talent.equals("Might")) {
 			Reaction.setText("You're no Ajax, but you can hold your own in a fight.");
 		} else if (talent.equals("Wits")) {
-			Reaction.setText("Its a strech to compare yourself to Daedalus, but\n you're certainly an\n intellegent individual.");
+			Reaction.setText("Its a strech to compare yourself to Daedalus, but \nyou're certainly an\nintellegent individual.");
 		}
 
 		if (general.equals("Diomedes") || general.equals("Odysseus")) {
-			Reaction.append("\n You have the day off, and choose to explore the camp during\n your freetime.");
+			Reaction.append("\nYou have the day off, and choose to explore the camp during \nyour freetime.");
 			exploreC();
 		} else if (general.equals("Agammemnon") || general.equals("Achilles")) {
-			Reaction.append("\n It is time for you to join the battlefield, and\n take arms agains the Trojains.");
+			Reaction.append("\nIt is time for you to join the battlefield, and take arms \nagains the Trojains.");
 			firstCombat();
 		}
 	}
 
 	public void exploreC() {
-
+		currentChoice = 5;
+		choice1.setText("N/A");
+		choice2.setText("N/A");
+		choice3.setText("N/A");
+		choice4.setText("N/A");
 	}
 
 	public void firstCombat() {
+		Reaction.append("\nYou find yourself engaged in man on man combat with a Trojan.\nHe lashes out at you with a sword. What do you do?");
 
+		choice1.setText("Block");
+		choice2.setText("Dodge");
+		choice3.setText("Scream in Fear");
+		choice4.setText("N/A");
 		// Combat description and trojan attack, 4 defensive options
 	}
 
 	public void counterAttack() {
 		// 4 offensive options
+		Reaction.append("\nNow the ball is in your court. Time to strike.");
+
+		choice1.setText("Feint");
+		choice2.setText("Overpower");
+		choice3.setText("Nimbly Slip By");
+		choice4.setText("N/A");
 	}
 }
